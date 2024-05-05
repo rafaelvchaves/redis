2a726e480c446b96723c26ba06ee25313282eb69
Rafael Chaves <rafaelvchaves@gmail.com>
2024-05-04 18:33:42 -0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -12,9 +12,15 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = listener.Accept()
+	defer listener.Close()
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+	if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
+		fmt.Println("Error writing bytes: ", err.Error())
 		os.Exit(1)
 	}
 }
